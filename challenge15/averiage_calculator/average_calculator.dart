5179a48af735705e89a9c60a64a262945bff0a32
Hossam <hossamibrahim2020@gmail.com>
2020-07-20 22:13:54 +0200

    16

---
+++ challenge15/averiage_calculator/average_calculator.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+class Average {
+  int number1, number2, number3 ,sumOfNumbers;
+  double result;
+  void averageCalculator() {
+    try {
+      stdout.write('Enter first number: ');
+      number1 = int.parse(stdin.readLineSync());
+      stdout.write('Enter second number: ');
+      number2 = int.parse(stdin.readLineSync());
+      stdout.write('Enter third number: ');
+      number3 = int.parse(stdin.readLineSync());
+      sumOfNumbers =number1 + number2 +number3;
+      result = sumOfNumbers / 3;
+      print('average is $result');
+    } catch (e) {}
+  }
+}
---
+++ challenge15/main.dart
@@ -0,0 +1,9 @@
+/*
+find average of 3 numbers that user enter
+*/
+import 'package:flutterinnative/challenge15/averiage_calculator/average_calculator.dart';
+
+final Average average = Average();
+main(){
+  average.averageCalculator();
+}
