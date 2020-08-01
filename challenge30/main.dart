13918a99790d2bf23b8c88f890e32a2e10100ea2
hossam <hossamibra18@gmail.com>
2020-08-01 18:22:18 +0200

    Rock , paper and Scissors game

---
+++ challenge30/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutterchallenges/siccors_rock_paper/view/game_viewmodel.dart';
+
+main(){
+  GameViewModel game = GameViewModel();
+  game.computerGuess();
+}
---
+++ challenge30/model/abstract_optoins.dart
@@ -0,0 +1,6 @@
+import 'dart:math';
+
+abstract class Options {
+  List<String> options = ['r', 'p', 's'];
+  Random random = Random();
+}
